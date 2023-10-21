eb4e741fe8434b7be903f99225e282afbb6c4a01
Mariaortiz14 <myito1612@gmail.com>
2023-10-21 14:47:36 -0500

    documentacion del codigo

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
+//modulos a exportar
 require('colors');
 const fs = require('fs');
 
-
+//definimos una constante datos archivos que va a exportar la carpeta de datos JSON
 const datosArchivo =require('./datos.json');
+/*definimos una funcion asincronica esto nos sirve para ejecutar funciones de manera asíncrona
+ Esto permite que la función no bloquee la ejecución del código principal y, por lo tanto, el buen 
+ funcionamiento 
+*/
 async function main() {
   console.clear();
   console.log('***************************'.blue);
   console.log('*'.blue+'       PROYECTO CLASES   *'.blue);
   console.log('****************************\n'.blue);
 
-
+//Definimos la clase producto
   class Producto {
     #codigoProducto;
     #nombreProducto;
     #inventarioProducto;
     #precioProducto;
-
+//utilizamos el metodo constructor
     constructor() {
       this.#codigoProducto = '';
       this.#nombreProducto = '';
       this.#inventarioProducto = 0;
       this.#precioProducto = 0;
     }
-
+//definimos metodos setter y getter
     set setCodigoProducto(value) {
       this.#codigoProducto = value;
 
@@ -56,21 +61,22 @@ async function main() {
       return this.#precioProducto;
     }
   }
-
+//definimos la clase productos tienda
   class ProductosTienda{
     #listaProductos;
-
+//utilizamos el metodo constructor
     constructor() {
       this.#listaProductos = [];
     }
-
+//metodos setter y getter
     get getListaProductos(){
       return this.#listaProductos;
     }
-
-    cargaArchivosProductos() {
-
+//definimos el metodo que es de productos tienda llamado cargar archivos productos
+     cargaArchivosProductos(){
+//utilizamos un contador
       let contador = 0;
+      //usamos el if-else para determinar si el ciclo se cumple
       if (datosArchivo.length > 0) {
         datosArchivo.forEach(objeto => {
           contador++;
@@ -87,23 +93,25 @@ async function main() {
       }
       console.log(`Total de productos cargados ==> `.blue + `${contador}`.red);
     }
-
-    grabaArchivosProductos() {
+//usamos el metodo graba archivos productos
+    grabaArchivosProductos(){
+/*Esta parte del código se utiliza para crear múltiples instancias de una clase en base 
+      a un conjunto de datos (en este caso, los productos)*/
       const instanciaClaseAObjetos = this.getListaProductos.map(producto => {
         return {
           codigoProducto: producto.getCodigoProducto,
-          nombreProducto: producto.nombreProducto,
+          nombreProducto: producto.getnombreProducto,
           inventarioProducto: producto.getInventarioProducto,
           precioProducto: producto.getPrecioProducto
         };
       });
-
+// convertir de array a cadena JSON
       const cadenaJson = JSON.stringify(instanciaClaseAObjetos, null, 2);
-
+//variable con el nombre del archivo
       const nombreArchivo = 'datos.json';
-
+//grabar la cadena JSON en el archivo
       fs.writeFileSync(nombreArchivo, cadenaJson, 'UTF-8');
-
+//mensaje que se muestra en la consola
       console.log(`DATOS GUARDADOS EN ${nombreArchivo}`.violet);
 
     }
@@ -118,6 +126,9 @@ async function main() {
     }
 
   }
+
+  /*Esta parte del código crea una nueva instancia de la clase ProductosTienda y
+   la almacena en la variable productosTienda. */
   let productosTienda = new ProductosTienda;
 
   productosTienda.cargaArchivosProductos();
